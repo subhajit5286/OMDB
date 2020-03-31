7c06983e89bc9dd0fc70997a5edd5a418f7e6163
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-03-31 10:31:23 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+
+var app = express();
+
+var request = require('request');
+
+var bodyParser = require('body-parser');
+
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public')); //serve(expr) the content of the public dir
+
+
+app.get("/", function(req, res){
+  var data = []; //empty array
+  res.render("home", {data: data});
+});
+app.post("/search", function(req, res){
+  var userQuery = req.body.searchedQuery;
+  request('https://www.omdbapi.com/?s=' + userQuery + '&type=movie&apikey=fc54ac14', function (error, response, body) {
+      if(error) console.log('StatusCode:', response && response.statusCode,'\nError:', error); // Print the error and the response status code if a response was received
+      else { 
+        var data = JSON.parse(body).Search;
+        if(JSON.parse(body).totalResults) {
+            res.render("home", {data: data});
+        }
+        else { //if there are no results
+            res.render("notfound", {userQuery: userQuery});
+        }
+    }
+  });
+});
+
+app.get("*", function(req, res){
+  res.render('404');
+});
+
+var port = process.env.PORT || 8080;
+
+
+app.listen(port, function(){
+  
+  console.log('Movie started on https://localhost:' + port); 
+});
+
+
